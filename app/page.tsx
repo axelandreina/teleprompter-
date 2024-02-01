c495d0269251ecf4ea9d5cdeed4acbef3a9d3d36
axel-tixcode <axelandre91@gmail.com>
2024-01-31 23:14:10 -0300

    initial layout and functionality

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <html lang="en" >
+      <head>
+        <link rel="icon" href="/memoji_smile.png" />
+      </head>
+      
+    </html>
+  );
+}
